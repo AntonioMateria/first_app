e41766d5d3558f98d5757959c1e4affd2610e8fc
Antonio <antoniomateria04@gmail.com>
2025-02-06 11:58:22 -0500

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,12 @@
+import 'package:first_app/Gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 12, 1, 39),
-                Color.fromARGB(255, 12, 203, 54),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text('Hello World!',
-            style:TextStyle(
-              color:Colors.white,
-              fontSize: 34,
-            ),
-  
-          ),
-        )
-    );
-  }
 }
